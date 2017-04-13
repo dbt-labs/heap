6208277cef31efb658afb4a88869e6b9fc255f91
Tristan Handy <jthandy.gmail.com>
2017-04-13 17:44:03 -0400

    deduplicated users because of a heap bug

--- models/base/heap_users.sql
+++ models/base/heap_users.sql
@@ -1,2 +1,13 @@
-select *
+{%
+    set window_clause = "partition by user_id order by last_modified rows
+        between unbounded preceding and unbounded following"
+%}
+
+--this is kinda bullshit--it's only done because heap has a bug where it inserts duplicate records for users.
+
+select distinct
+  user_id,
+  last_value("identity") over ( {{window_clause}} ) as "identity",
+  last_value(lower(email)) over ( {{window_clause}} ) as email,
+  min(joindate) over ( {{window_clause}} ) as joindate
 from {{var('users_table')}}
