d61339aace0eb4e10a544dcb7337a7fef3057d91
Amy Chen <yuanamychen@gmail.com>
2019-11-12 16:46:46 -0500

    update naming for referrer medium

--- models/transform/heap_sessions_xf.sql
+++ models/transform/heap_sessions_xf.sql
@@ -66,7 +66,7 @@ with sessions as (
         as user_sessionidx,
       ea.session_end_time,
       ea.event_count,
-      referrers.{{ adapter.quote('medium') }} as medium,
+      referrers.{{ adapter.quote('medium') }} as referrer_medium,
       referrers.{{ adapter.quote('source') }} as referrer_source,
       coalesce(users.user_identity, s.user_id::varchar) as blended_user_id,
       {{ dbt_utils.get_url_parameter('ea.first_page_query', 'gclid') }} as gclid
