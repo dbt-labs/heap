0d403c8e133c7b1ff435d3c6d3b6b1c8afc02d9f
David Oliver <doliver@aclu.org>
2020-04-20 12:54:52 -0400

    Update package to include joindate field and redo project config

--- models/transform/heap_sessions_xf.sql
+++ models/transform/heap_sessions_xf.sql
@@ -18,7 +18,7 @@ with sessions as (
 ), events as (
 
     select * from {{ref('heap_events')}}
-    
+
     {% if is_incremental() %}
     where event_time >= (select dateadd(hour, -5, max(session_start_time)) from {{this}})
     {% endif %}
@@ -33,36 +33,37 @@ with sessions as (
 
 ), events_agg as (
 
-    select distinct 
-        
+    select distinct
+
         session_id,
         max(event_time) over (partition by session_id) as session_end_time,
         count(*) over (partition by session_id) as event_count,
-        
+
         first_value(query) over (
-            partition by session_id 
-            order by event_time 
+            partition by session_id
+            order by event_time
             rows between unbounded preceding and unbounded following
             ) as first_page_query
-            
+
     from events
 
 ), referring_domains as (
 
     select
-    
+
         *,
         ltrim({{dbt_utils.get_url_host('referrer')}}, 'www.')
             as referring_domain
-        
+
     from sessions
 
 ), joined as (
 
     select
-    
+
       s.*,
-      row_number() over (partition by s.user_id order by s.session_start_time) 
+      users.joindate as user_joindate,
+      row_number() over (partition by s.user_id order by s.session_start_time)
         as user_sessionidx,
       ea.session_end_time,
       ea.event_count,
@@ -70,7 +71,7 @@ with sessions as (
       referrers.{{ adapter.quote('source') }} as referrer_source,
       coalesce(users.user_identity, s.user_id::varchar) as blended_user_id,
       {{ dbt_utils.get_url_parameter('ea.first_page_query', 'gclid') }} as gclid
-      
+
     from referring_domains s
       left outer join events_agg ea on s.session_id = ea.session_id
       left outer join referrers on s.referring_domain = referrers.{{ adapter.quote('domain') }}
